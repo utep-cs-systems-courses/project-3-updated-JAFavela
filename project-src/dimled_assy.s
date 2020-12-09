bd60cbf14e3ff7aa57e09f8c6b3353420577c2c3
JAFavela <jafavela@miners.utep.edu>
2020-12-09 15:30:41 -0700

    last one, added some comments

--- project-src/dimled_assy.s
+++ project-src/dimled_assy.s
@@ -1,36 +1,35 @@
 
 	.data
 ledState:
-	.word 0
+	.word 0			;Write static variable ledState to data with a 0
 	
 	.text
-jt:
-	.word case0
-	.word case1
-	.word case2
-	.word case3
-	.word case4
-	.word case5
-	.word default
+jt:				;Write jump table to text
+	.word case0		
+	.word case1		
+	.word case2		
+	.word case3		
+	.word case4		
+	.word case5		
+	.word default		
+
 	
 	.local	state
 	.balign 2
-	.global	dim_rg_led
+	.global	dim_rg_led	;Make function callable to other files
+dim_rg_led:			;Start of function
+	mov.b	&ledState, r12	;Move the address of ledState into r12 r12=ledState
+	cmp	#6, r12		;if r12>=0
+	jhs	default		;goto default
 
-	
-dim_rg_led:
-	mov.b	&ledState, r12
-	cmp	#6, r12
-	jhs	default
-	add.b	r12,r12
-	mov	jt(r12), r0	
+	add.b	r12,r12		;Index into JT
+	mov	jt(r12), r0	;Jump to case indicated by the value in r12 (*2 for offset)
 case0:
 	mov.b	#0, &red_on
 	mov.b	&ledState, r12
 	mov.b	#1, r12
 	and	#0xff, r12
 	mov.b	r12, &ledState
-	jmp	default
 case1:
 case2:
 case3:
